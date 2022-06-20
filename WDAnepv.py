408fc49722207e15e92da918c1739b8aafdda6ac
gnodking7 <91911643+gnodking7@users.noreply.github.com>
2022-06-20 16:14:11 -0700

    Update WDAnepv.py

--- WDAnepv.py
+++ WDAnepv.py
@@ -10,59 +10,9 @@ Nonlinear Eigenvalue based Algorithm: WDAnepv
 
 import numpy as np
 from scipy import linalg
+import WDA_subfunc as sub
 
 ###########################################################################
-
-def sinkhorn(K, tol = 1e-5, maxitr = 10):
-    """
-    Performs Sinkhorn-Knopp Iteration on a positive matrix K to obtain
-    positive vectors u and v such that diag(u) * K * diag(v) is normalized doubly stochastic
-
-    Article on Sinkhorn iteration:
-    @article{sinkhorn1967diagonal,
-        title={Diagonal equivalence to matrices with prescribed row and column sums},
-        author={Sinkhorn, Richard},
-        journal={The American Mathematical Monthly},
-        volume={74},
-        number={4},
-        pages={402--405},
-        year={1967},
-        publisher={JSTOR}
-    }
-
-    PARAMETERS
-    ----------
-    K :         ndarray, shape (n, m)
-                Positive matrix
-    tol :       float, optional, default set to 1e-5
-                Tolerance parameter for stopping criteria
-    maxitr :    int, optional, default set to 10
-                Number of maximum number of iterations
-
-    RETURNS
-    -------
-    u :         ndarray, shape (n,)
-                Optimal left vector
-    v :         ndarray, shape (m,)
-                Optimal right vector
-    v_err :     list
-                List of 2-norm errors between consecutive v vectors
-    """
-
-    n, m = K.shape[0], K.shape[1]
-    vk = np.ones(m) / m # initial point
-    v_err = []
-    # updates
-    for i in range(maxitr):
-        uk = np.ones(n) / (np.dot(K, vk)) / n
-        new_vk = np.ones(m) / (np.dot(K.T, uk)) / m
-        v_err.append(linalg.norm(new_vk - vk))   # error
-        vk = new_vk
-        if v_err[i] < tol:
-            break
-    u = uk
-    v = vk
-    return u, v
 
 def Dink_TR(A, B, k, X, tol = 1e-5, maxitr = 100):
     """
@@ -106,6 +56,10 @@ def Dink_TR(A, B, k, X, tol = 1e-5, maxitr = 100):
                 List of TRopt values
     Err :       list
                 List of TRopt errors between consecutive projections
+    Sub_Err :   list
+                List of subspace errors between consecutive projections
+    PROJ:       list
+                List of projections matrices
     itr :       int
                 Number of iterations
     """
@@ -113,116 +67,47 @@ def Dink_TR(A, B, k, X, tol = 1e-5, maxitr = 100):
     n = len(A)
     q = np.trace(np.matmul(X.T, np.matmul(A, X))) / np.trace(np.matmul(X.T, np.matmul(B, X)))
     Q, Err = [], []
+    Sub_Err = []
+    PROJ = []
     for i in range(maxitr):
         C = A - q * B
         C = (C + C.T) / 2   # ensure it's symmetric
 
-        #D, V = linalg.eigh(C, subset_by_index = [n - k, n - 1]) # k largest eigs and eigenvectors, in ascending order
-        #newX = np.fliplr(V) # eigenvectors now in descending order
-
         D, V = linalg.eigh(C)
         idx = np.argsort(-D)
         newX = V[:, idx[0:k]]
-
         newq = np.trace(np.matmul(newX.T, np.matmul(A, newX))) / np.trace(np.matmul(newX.T, np.matmul(B, newX)))
 
         err = np.abs(newq - q) / np.abs(newq)
         Err.append(err)
-        # stopping criteria
-        if (abs(linalg.subspace_angles(X, newX)[0]) < tol) or (err < 1e-5):
-            X = newX
-            Q.append(newq)
-            break
+        sub_err = linalg.subspace_angles(X, newX)[0]
+        Sub_Err.append(sub_err)
         # Update the iterate
         Q.append(q)
+        PROJ.append(X)
         q = newq
         X = newX
+        # stopping criteria
+        if (Sub_Err[i] < tol) or (err < 1e-5):
+            Q.append(q)
+            PROJ.append(X)
+            break
     itr = i
-    return X, Q, Err, itr
+    return X, Q, Err, Sub_Err, PROJ, itr + 1
 
-def split_classes(X, y):
+def wda_nepv(DATA, LABEL, p, P, lamb=0.01, e=0, k=10, tol=1e-3, maxitr=50):
     """
-    Split data samples in data matrix X by classes using the labels y
-
-    PARAMETERS
-    ----------
-    X :     ndarray, shape (n, d)
-            Data matrix
-    y :     ndarray, shape (n,)
-            Labels for X
-
-    RETURNS
-    -------
-    X_c :   list
-            List of data class matrices
-    """
-
-    labels = np.unique(y)
-    X_c = [X[y == i, :].astype(np.float32) for i in labels]
-    return X_c
-
-def dist(X, Y):
-    """
-    Compute squared Euclidean distance between
-    all possible data points in data matrices X and Y
-
-    PARAMETERS
-    ----------
-    X :     ndarray, shape (n, d)
-            Data matrix
-    Y :     ndarray, shape (m, d)
-            Data matrix
-
-    RETURNS
-    -------
-    M :     ndarray, shape (n, m)
-            Metric matrix
-    """
-
-    X_inn_prod = np.sum(np.square(X), 1)
-    Y_inn_prod = np.sum(np.square(Y), 1)
-    M = X_inn_prod.reshape((-1, 1)) + Y_inn_prod.reshape((1, -1)) - 2 * np.matmul(X, Y.T)
-    return M
-
-
-def pair_tensor(T, X, Y):
-    """
-    Computes the sum of rank one matrices
-        \sum_{ij}T(i,j) * X(i,:)' * Y(j,:)
-
-    PARAMETERS
-    ----------
-    T :     ndarray, shape (n, m)
-            Cost matrix, obtained from Sinkhorn
-    X :     ndarray, shape (n, d)
-            Data matrix
-    Y :     ndarray, shape (m, d)
-            Data matrix
-
-    RETURNS
-    -------
-    C_XY :  ndarray, shape (d, d)
-            Matrix
-    """
-    d = X.shape[1]
-    temp = X[:, None] - Y
-    C = temp * np.sqrt(T)[:, :, None]
-    C = C.reshape((-1, d))
-    C_XY = np.matmul(C.T, C)
-    return C_XY
-
-def wda_nepv(DATA, LABEL, p, P, lamb=0.01, e=0, k=10, tol=1e-5, maxitr=50):
-    """
-    Performs Wasserstein Discriminant Analysis via Bi-Level Optimization method.
-    Inner optimization involves solving Trace Ratio optimization (TRopt) using
-    Self-Consistent-Field method (equivalent to Dinkelbach's algorithm).
-
     Mathematical formulation of WDA is:
         \max_{P^TP=I} Trace(P^TC_b(P)P)/Trace(P^TC_w(P)P)
     where C_b(P) and C_w(P) are sum of large number of rank one matrices of the form t*d*d'
     Rather than summing large number of times, it is far more efficient to
     form a (short and long) matrix with \sqrt{t}d columns then do matrix
     times matrix transpose to form C_b(P) and C_w(P).
+
+    Performs Wasserstein Discriminant Analysis via Bi-Level Optimization method:
+        P_{k+1} = \argmax_{P^TP=I} Trace(P^TC_b(P_k)P)/Trace(P^TC_w(P_k)P)
+    Above inner optimization involves solving Trace Ratio optimization (TRopt) using
+    Self-Consistent-Field method (equivalent to Dinkelbach's algorithm).
 
     PARAMETERS
     ----------
@@ -255,17 +140,29 @@ def wda_nepv(DATA, LABEL, p, P, lamb=0.01, e=0, k=10, tol=1e-5, maxitr=50):
                 List of Frobenius errors between consecutive projections
     WDA_Val :   list
                 List of WDA values
+    Dink_Val :  list
+                List of inner standard trace ratio (Dinkelbach) values
     proj :      callable
                 Projection function
+    itr :       int
+                Number of iterations
+    DINK_PROJ : list
+                List of inner trace ratio (Dinkelbach) projections matrices
+    PROJ :      list
+                List of projections matrices
     """
 
-    #mx = np.mean(DATA)
-    #DATA -= mx.reshape((1, -1))
+    mx = np.mean(DATA)
+    DATA -= mx.reshape((1, -1))
 
     d = DATA.shape[1]   # data point dimension
     Sub_Err, Norm_Err, WDA_Val = [], [], []
+    ITR = []
+    Dink_Val = []
+    DINK_PROJ = []
+    PROJ = []
 
-    X_c = split_classes(DATA, LABEL) # split data by classes
+    X_c = sub.split_classes(DATA, LABEL) # split data by classes
 
     for it in range(maxitr):
         # obtain between scatter A:=C_b(P) and within scatter B:=C_w(P)
@@ -275,32 +172,39 @@ def wda_nepv(DATA, LABEL, p, P, lamb=0.01, e=0, k=10, tol=1e-5, maxitr=50):
             pxi = np.matmul(xi, P)
             for j, xj in enumerate(X_c[i:]):
                 pxj = np.matmul(xj, P)
-                M = dist(pxi, pxj)
+                M = sub.dist(pxi, pxj)
                 K = np.exp(-lamb * M)
-                u, v = sinkhorn(K, 1e-5, k)
+                u, v = sub.Acc_SK(K, 1e-5, k)
                 T = u.reshape((K.shape[0], 1)) * K * v.reshape((1, K.shape[1]))
                 if j == 0:
-                    B += pair_tensor(T, xi, xj)
+                    B += sub.pair_tensor(T, xi, xj)
                 else:
-                    A += pair_tensor(T, xi, xj)
+                    A += sub.pair_tensor(T, xi, xj)
         B += e * np.eye(d)  # perturb
         # store WDA value
+        A = (A + A.T) / 2
+        B = (B + B.T) / 2
         WDA_Val.append(np.trace(np.matmul(P.T, np.matmul(A, P))) / np.trace(np.matmul(P.T, np.matmul(B, P))))
         # solve TRopt \max_{P^TP=I} Trace(P^TAP)/Trace(P^TBP) by Dinkelbach's iteration
-        NEW_P, Q, Err, itr = Dink_TR(A, B, p, P, tol=1e-3)
+        NEW_P, Q, Err, Dink_Sub_Err, Dink_PROJ, itr = Dink_TR(A, B, p, P, tol=1e-3)
+        Dink_Val += Q
+        DINK_PROJ += Dink_PROJ
+        PROJ.append(P)
         # errors
         Sub_Err.append(abs(linalg.subspace_angles(P, NEW_P)[0]))
         Norm_Err.append(np.linalg.norm(P - NEW_P, 'fro') / np.linalg.norm(NEW_P, 'fro'))
         # update
         P = NEW_P
+        ITR.append(itr)
         # stopping criteria
         if it > 0:
             err = abs(WDA_Val[it] - WDA_Val[it - 1]) / abs(WDA_Val[it])
             if (Sub_Err[it] < tol) or (Norm_Err[it] < tol) or (err < 1e-5):
+                PROJ.append(P)
+                WDA_Val.append(np.trace(np.matmul(P.T, np.matmul(A, P))) / np.trace(np.matmul(P.T, np.matmul(B, P))))
                 break
 
     def proj(X):
-        return np.matmul(X, P)
-        #return (X - mx.reshape((1, -1))).dot(P)
+        return (X - mx.reshape((1, -1))).dot(P)
 
-    return P, Sub_Err, Norm_Err, WDA_Val, proj
+    return P, Sub_Err, Norm_Err, WDA_Val, Dink_Val, proj, ITR, DINK_PROJ, PROJ
