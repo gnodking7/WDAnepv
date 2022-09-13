d3803e84b95d8e9f1a235210dfffed4fb2797fed
gnodking7 <91911643+gnodking7@users.noreply.github.com>
2022-09-12 19:20:55 -0700

    Update WDAnepv.py

--- WDAnepv.py
+++ WDAnepv.py
@@ -162,7 +162,7 @@ def wda_nepv(X, y, p, reg, P0, Breg=0, k=10, maxitr=100, tol=1e-5):
                 pxj = np.matmul(xj, P)
                 M = sub.dist(pxi, pxj)
                 K = np.exp(-lamb * M)
-                u, v = sub.Acc_SK(K, 1e-5, k)
+                u, v, Err = sub.Acc_SK(K, 1e-5, k)
                 T = u.reshape((K.shape[0], 1)) * K * v.reshape((1, K.shape[1]))
                 if j == 0:
                     B += sub.pair_tensor(T, xi, xj)
